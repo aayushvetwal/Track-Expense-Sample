56367f477c07efe5f56ef7e04a82249358e4ec41
Aayush Vetwal <aayush@herkimer.maxinc.com>
2018-05-22 15:38:18 -0400

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-				'sass-loader'
-			]
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	}
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	const CSSExtract = new ExtractTextPlugin('styles.css');
+	
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: CSSExtract.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					]
+				})
+			}]
+		},
+		plugins: [
+			CSSExtract
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		}
+	};
 };
 
 //loader - lets you customize the behavior of webpack when it loads fileSize (used in module)
 // use - allows us to use array of loaders
+// plugins stores third party plugins
