aa3ecf46f44340a75f439a7540afcc91dd5489e4
Aayush Vetwal <aayush@herkimer.maxinc.com>
2018-06-28 17:35:24 -0400

    Completed Styling

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import {startSetExpenses} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import {login, logout} from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
+import LoadingPage from './components/LoadingPage';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -41,7 +42,7 @@ const renderApp = () => {
 	}
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
 	if(user){
